5f9b08eae29d726e5b9cfc540898277506000b76
fred <fred@fred-nb.sot.com>
2018-04-27 15:53:49 +0300

    Simplify pubSubStoreInterface

--- src/pubsub-store.ts
+++ src/pubsub-store.ts
@@ -3,20 +3,33 @@ import {CentrifugoClient, CentrifugoClientOptions} from "graphql-centrifugo-clie
 
 export class PubSubStore {
     private store = new Map<string, CentrifugoPubSub>();
+    private centrifugoClientOptions: CentrifugoClientOptions;
+    private keyPrefix: string;
 
-    public get(options: CentrifugoClientOptions): CentrifugoPubSub {
-        if (!this.hasPubsub(options.id)) {
+    public constructor(options: CentrifugoClientOptions, keyPrefix: string = '') {
+        this.centrifugoClientOptions = options;
+        this.keyPrefix = keyPrefix;
+    }
+
+    public get(key: string = 'guest'): CentrifugoPubSub {
+        key = this.prepareKey(key);
+
+        if (!this.hasPubsub(key)) {
+            let options = this.centrifugoClientOptions;
+            options.id = key;
+
             const centrifugoClient = new CentrifugoClient(options);
             const pubSubOptions = {
                 centrifugoClient,
                 onEmptySubscribers: this.removePubsub.bind(this),
             };
+
             const centrifugoPubSub = new CentrifugoPubSub(pubSubOptions);
 
-            this.setPubsub(options.id, centrifugoPubSub);
+            this.setPubsub(key, centrifugoPubSub);
         }
 
-        return this.getPubsub(options.id);
+        return this.getPubsub(key);
     }
 
     private getPubsub(key: string): CentrifugoPubSub {
@@ -35,5 +48,9 @@ export class PubSubStore {
         if (this.store.has(key)) {
             this.store.delete(key);
         }
+    }
+
+    private prepareKey(key: string): string {
+        return this.keyPrefix ? this.keyPrefix + '_' + key : key;
     }
 }
